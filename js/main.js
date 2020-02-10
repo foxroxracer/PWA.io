6fef9b674034dbf5476ddc0e95d39ac021d360b9
foxroxracer <sanderdrupal@gmail.com>
2020-02-10 20:07:06 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/PWA/sw.js').then(function(registration) {
+        navigator.serviceWorker.register('/PWA.io/sw.js').then(function(registration) {
           // Registration was successful
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
