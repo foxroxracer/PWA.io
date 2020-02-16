2a0e7c691df83ac384b13f0dbbc37a7a265d647b
Sander <sanderdrupal@gmail.com>
2020-02-16 20:20:46 +0100

    Added quiz

---
+++ Quiz/precache-manifest.58f0cae312a1074ba81791197cea8ae9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68f452fadf2e11a62e0b",
+    "url": "/css/app.650718a8.css"
+  },
+  {
+    "revision": "0660f3a699e675e17c2b1f21fd80efdc",
+    "url": "/fonts/unisansdemo-heavycaps-webfont.0660f3a6.woff"
+  },
+  {
+    "revision": "c2bdccf16cbbec65f315a8ba400eb3e6",
+    "url": "/fonts/unisansdemo-heavycaps-webfont.c2bdccf1.woff2"
+  },
+  {
+    "revision": "89d7ec958785b7ea09eac0d47754b4ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68f452fadf2e11a62e0b",
+    "url": "/js/app-legacy.d6ba8f64.js"
+  },
+  {
+    "revision": "d65eb76270e86cc8c220",
+    "url": "/js/chunk-vendors-legacy.bb5831a7.js"
+  },
+  {
+    "revision": "fe0d077f5142acf14d374be038fa65a0",
+    "url": "/manifest.json"
+  }
+]);
---
+++ Quiz/precache-manifest.828e95128b40232623e1db5b28cafb4f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33985e9de8f973186c78",
+    "url": "/css/app.650718a8.css"
+  },
+  {
+    "revision": "0660f3a699e675e17c2b1f21fd80efdc",
+    "url": "/fonts/unisansdemo-heavycaps-webfont.0660f3a6.woff"
+  },
+  {
+    "revision": "c2bdccf16cbbec65f315a8ba400eb3e6",
+    "url": "/fonts/unisansdemo-heavycaps-webfont.c2bdccf1.woff2"
+  },
+  {
+    "revision": "dcfac3304245515a028aa2f835207002",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33985e9de8f973186c78",
+    "url": "/js/app.e5eb07cd.js"
+  },
+  {
+    "revision": "c25e99a20ccddbdccd05",
+    "url": "/js/chunk-vendors.a8957ca7.js"
+  },
+  {
+    "revision": "fe0d077f5142acf14d374be038fa65a0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
